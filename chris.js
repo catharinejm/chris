8f81634963a84b4a450417caaec9c26e9ce9b12c
Chris Distad <hl2ep3@live.com>
2011-03-29 13:41:18 -0400

    add moving triangles and more circles

--- chris.js
+++ chris.js
@@ -8,6 +8,11 @@ KEY_W = 87;
 KEY_1 = 49;
 KEY_2 = 50;
 KEY_S = 83;
+Z = 90;
+X = 88;
+C = 67;
+V = 86;
+KEY_3 = 51;
 
 $(function() {
     var windowDiv = $("#window");
@@ -16,6 +21,15 @@ $(function() {
     var y = 100;
     var radius = 30;
     var fill = "white";
+    var a = 400;
+    var b = 325;
+    var c = 50;
+    var fill2 = 'orange';
+    var cc = 10;
+    var fill3 = 'green';
+    var rotate_angle = 0;
+    var spin = 0;
+    var spin2 = 0;
 
     draw();
 
@@ -43,14 +57,31 @@ $(function() {
 	    y = y - 10;
 	} else if (event.which == KEY_S) {
 	    fill = "white";
+	} else if (event.which == Z) {
+	    a = a + 10;
+	} else if (event.which == X) {
+	    a = a - 10;
+	} else if (event.which == KEY_3) {
+	    rotate_angle += 45;
 	}
-	draw();
     });
+
+    setInterval(function() {
+	spin += 6;
+	spin2 -= 6;
+	draw();
+    }, 16);
 
     function draw() {
 	canvas.clear();
 	canvas.circle(x, y, radius).attr('fill', fill);
-	canvas.path("M 200 200 L 400 200").attr('stroke', "blue");
-	canvas.path("M 300 400 L 500 200").attr("stroke", "green");
+	canvas.circle(a, b, c).attr('fill', fill2);
+	canvas.circle(a, b, cc).attr('fill', fill3);
+	canvas.path("M 0 200 L 400 200").attr('stroke', "blue");
+	canvas.path("M 400 0 L 400 50").attr("stroke", "blue");
+	canvas.path("M 400 150 L 400 200").attr("stroke", "blue");
+	canvas.path('M 400 200 L 500 400 L 300 400 z').attr('stroke', 'blue').rotate(rotate_angle, 400, 200);
+	canvas.path("M 200 50 L 225 75 L 175 75 z").attr({stroke: "red", "stroke-width": 3, fill: "blue"}).rotate(spin, 200, 50);
+	canvas.path("M 200 150 L 225 175 L 175 175 z").attr("stroke", "blue").rotate(spin2, 200, 150);
     }
 });
